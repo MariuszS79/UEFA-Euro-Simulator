819e848df0efe78b1bf52bf69f8d46f9c3dcf3ff
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2021-07-14 01:17:13 +0100

    Update Euro.py

--- Euro.py
+++ Euro.py
@@ -5,4 +5,37 @@ uefa_member_countries=["Albania", "Andorra", "Armenia", "Austria", "Azerbaijan",
 teams=random.sample(set(["Albania", "Andorra", "Armenia", "Austria", "Azerbaijan", "Belarus", "Belgium", "Bosnia and Herzegovina", "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark", "England", "Estonia", "Faroe Islands", "Finland", "France", "Georgia", "Germany", "Gibraltar", "Greece", "Hungary", "Iceland", "Israel", "Italy", "Kazakhstan", "Kosovo", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg", "Malta", "Moldova", "Montenegro", "Netherlands", "North Macedonia", "Northern Ireland", "Norway", "Poland", "Portugal", "Republic of Ireland", "Romania", "Russia", "San Marino", "Scotland", "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Turkey", "Ukraine", "Wales"]), 24)
 
 teams.sort()
-print (*teams, sep="\n")
+print (*teams, sep=",")
+random.shuffle(teams)
+
+group_a=[]
+group_b=[]
+group_c=[]
+group_d=[]
+group_e=[]
+group_f=[]
+
+def make_groups():
+  for i in range (4):
+    group_a.append(teams.pop(-1))
+    group_b.append(teams.pop(-1))
+    group_c.append(teams.pop(-1))
+    group_d.append(teams.pop(-1))
+    group_e.append(teams.pop(-1))
+    group_f.append(teams.pop(-1))
+    
+    
+
+make_groups()
+print("\nGroup A:")
+print (*group_a, sep="\n")
+print("-----\nGroup B:")
+print (*group_b, sep="\n")
+print("-----\nGroup C:")
+print (*group_c, sep="\n")
+print("-----\nGroup D:")
+print (*group_d, sep="\n")
+print("-----\nGroup E:")
+print (*group_e, sep="\n")
+print("-----\nGroup F:")
+print (*group_f, sep="\n")
