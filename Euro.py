21bb7c64892da6965e2a1160c7ace9964821a48c
MariuszS79 <54702756+MariuszS79@users.noreply.github.com>
2021-07-21 23:42:05 +0100

    Update Euro.py

--- Euro.py
+++ Euro.py
@@ -5,41 +5,73 @@ from tabulate import tabulate
 uefa_member_countries=["Albania", "Andorra", "Armenia", "Austria", "Azerbaijan", "Belarus", "Belgium", "Bosnia and Herzegovina", "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark", "England", "Estonia", "Faroe Islands", "Finland", "France", "Georgia", "Germany", "Gibraltar", "Greece", "Hungary", "Iceland", "Israel", "Italy", "Kazakhstan", "Kosovo", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg", "Malta", "Moldova", "Montenegro", "Netherlands", "North Macedonia", "Northern Ireland", "Norway", "Poland", "Portugal", "Republic of Ireland", "Romania", "Russia", "San Marino", "Scotland", "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Turkey", "Ukraine", "Wales"]
 
 def pick24():
-    global teams
-    teams=random.sample(set(uefa_member_countries), 24)
-    teams.sort()
-    random.shuffle(teams)
+  global teams
+  teams=random.sample(set(uefa_member_countries), 24)
+  teams.sort()
+  random.shuffle(teams)
 
 
 year=2020
 
 
 def make_groups():
-    for i in range (4):
-      group_a.append(teams.pop(0))
-      group_b.append(teams.pop(0))
-      group_c.append(teams.pop(0))
-      group_d.append(teams.pop(0))
-      group_e.append(teams.pop(0))
-      group_f.append(teams.pop(0))
+  for i in range (4):
+    group_a.append(teams.pop(0))
+    group_b.append(teams.pop(0))
+    group_c.append(teams.pop(0))
+    group_d.append(teams.pop(0))
+    group_e.append(teams.pop(0))
+    group_f.append(teams.pop(0))
 
 def print_groups():
-    print("\nGroup A:")
-    print (*group_a, sep="\n")
-    print("-----\nGroup B:")
-    print (*group_b, sep="\n")
-    print("-----\nGroup C:")
-    print (*group_c, sep="\n")
-    print("-----\nGroup D:")
-    print (*group_d, sep="\n")
-    print("-----\nGroup E:")
-    print (*group_e, sep="\n")
-    print("-----\nGroup F:")
-    print (*group_f, sep="\n")
+  print("\nGroup A:")
+  print (*group_a, sep="\n")
+  print("-----\nGroup B:")
+  print (*group_b, sep="\n")
+  print("-----\nGroup C:")
+  print (*group_c, sep="\n")
+  print("-----\nGroup D:")
+  print (*group_d, sep="\n")
+  print("-----\nGroup E:")
+  print (*group_e, sep="\n")
+  print("-----\nGroup F:")
+  print (*group_f, sep="\n")
 
 score=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 7]
     
+#instert matches played to table
+def mp():
+    i[0][1] = i[0][1]+1
+    i[1][1] = i[1][1]+1
 
+#insert win, lose, draw and points to table
+def wld_pts():
+    if score1==score2:
+        i[0][3] = i[0][3]+1
+        i[1][3] = i[1][3]+1
+        i[0][8] = i[0][8]+1
+        i[1][8] = i[1][8]+1
+    elif score1>score2:
+        i[0][2] = i[0][2]+1
+        i[1][4] = i[1][4]+1
+        i[0][8] = i[0][8]+3
+        i[1][8] = i[1][8]+0
+    elif score1<score2:
+        i[0][4] = i[0][4] + 1
+        i[1][2] = i[1][2] + 1
+        i[0][8] = i[0][8] + 0
+        i[1][8] = i[1][8] + 3
+
+#insert gf ga and gd into table
+def gf_ga_gd():
+    i[0][5] = i[0][5] + score1
+    i[1][5] = i[1][5] + score2
+
+    i[0][6] = i[0][6] + score2
+    i[1][6] = i[1][6] + score1
+
+    i[0][7] = i[0][5] - i[0][6]
+    i[1][7] = i[1][5] - i[1][6]
 
 
 
@@ -73,19 +105,39 @@ while game:
 
     input("\nHit enter to see group stage")
     os.system('cls')
-    #os.system('clear')
+    os.system('clear')
 
-    print("Group A")
-
-
-    
+   #group A
 
     headers=["Team", "MP", "W", "D", "L", "GF", "GA", "GD", "Pts"]
     group_a[0]=[group_a[0], 0, 0, 0, 0, 0, 0, 0, 0]
     group_a[1]=[group_a[1], 0, 0, 0, 0, 0, 0, 0, 0]
     group_a[2]=[group_a[2], 0, 0, 0, 0, 0, 0, 0, 0]
     group_a[3]=[group_a[3], 0, 0, 0, 0, 0, 0, 0, 0]
+
+    grpa=[[group_a[0], group_a[1]], [group_a[2], group_a[3]], [group_a[0], group_a[2]],[group_a[1], group_a[3]], [group_a[0], group_a[3]], [group_a[1], group_a[2]]]
+    print("----------\nGroup A matches\n----------")
+    
+    for i in grpa:
+          score1=(random.choice(score))
+          score2=(random.choice(score))
+          mp()
+          wld_pts()
+          gf_ga_gd()
+          print(i[0][0], i[1][0], sep="   vs   ")
+          print(score1,":",score2)
+
+    group_a.sort(key=lambda team: (team[8], team[7]), reverse=True)
+    print("\n")
     print(tabulate(group_a, headers=["Team", "MP", "W ", "D ", "L ", "GF", "GA", "GD", "Pts"],))
+    
+    print ("\nTeams getting promoted are:", (group_a[0][0]),"and", group_a[1][0])
+
+    
+
+
+
+
 
 
 
